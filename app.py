205b6c256f62c0424e8c1ddce1a06df032fe8822
dawnsoup <atxm@163.com>
2019-05-16 15:41:03 +0800

    Add database support with Flask-SQLAlchemy

--- app.py
+++ app.py
@@ -1,25 +1,76 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy #导入扩展类
+import os
+import click
 
 app = Flask(__name__)
 
 
-name = 'txm'
 
-movies = [
-    {'title': 'My Neighbor Totoro', 'year':'1988'},
-    {'title': 'Dead Poets Society', 'year':'1989'},
-    {'title': 'A Perfect World', 'year':'1993'},
-    {'title': 'Leon', 'year':'1994'},
-    {'title': 'Mahjong', 'year':'1996'},
-    {'title': 'Swallowtail Butterfly', 'year':'1996'},
-    {'title': 'King of Comedy', 'year':'1999'},
-    {'title': 'Devils on the Doorstep', 'year':'1999'},
-    {'title': 'WALL-E', 'year':'2008'},
-    {'title': 'The Pork of Music', 'year':'2012'},
-]
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(app.root_path, 'watchlist_data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+db = SQLAlchemy(app)
+
+
+
+
+#############################################
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(60))
+    year = db.Column(db.String(4))
+
+#############################################
+
+@app.cli.command() #注册为命令
+@click.option('--drop', is_flag=True, help='Create after drop.')    #设置选项
+def initdb(drop):
+    """Initialize the database."""
+    if drop:    #判断是否输入了选项
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database.')
+
+
+@app.cli.command()
+def forge():
+    """Generate fake data."""
+    db.create_all()
+    name = 'txm'
+
+    movies = [
+        {'title': 'My Neighbor Totoro', 'year':'1988'},
+        {'title': 'Dead Poets Society', 'year':'1989'},
+        {'title': 'A Perfect World', 'year':'1993'},
+        {'title': 'Leon', 'year':'1994'},
+        {'title': 'Mahjong', 'year':'1996'},
+        {'title': 'Swallowtail Butterfly', 'year':'1996'},
+        {'title': 'King of Comedy', 'year':'1999'},
+        {'title': 'Devils on the Doorstep', 'year':'1999'},
+        {'title': 'WALL-E', 'year':'2008'},
+        {'title': 'The Pork of Music', 'year':'2012'},
+    ]
+    
+    user = User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m['title'], year=m['year'])
+        db.session.add(movie)
+
+    db.session.commit()
+    click.echo('Done.')
+
 
 @app.route('/')
 def index():
-    return render_template('index.html', name=name, movies=movies)
+    user = User.query.first()
+    movies = Movie.query.all()
+
+    return render_template('index.html', user=user, movies=movies)
 
 
